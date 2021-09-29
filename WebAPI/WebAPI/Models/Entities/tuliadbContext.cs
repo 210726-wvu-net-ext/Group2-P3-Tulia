4e6e8f1b31457d977124ce345f513fa207586faf
sloanliam <sloanliam1091@gmail.com>
2021-09-29 17:44:30 -0400

    removed "on-configuring" method again

--- WebAPI/WebAPI/Models/Entities/tuliadbContext.cs
+++ WebAPI/WebAPI/Models/Entities/tuliadbContext.cs
@@ -24,15 +24,6 @@ namespace WebAPI.Models.Entities
         public virtual DbSet<Report> Reports { get; set; }
         public virtual DbSet<User> Users { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:rev-yyarytskyy.database.windows.net,1433;Initial Catalog=tulia-db;Persist Security Info=False;User ID=petadmin;Password=R@spberryPi4;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
