1e1c9bf3c74a10384cded7303f2fc95979969b5d
sloanliam <sloanliam1091@gmail.com>
2021-10-13 14:28:49 -0400

    Added 2nd test

--- WebAPI/APITesting/UnitTest1.cs
+++ WebAPI/APITesting/UnitTest1.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Threading.Tasks;
+using WebAPI.Models;
+using WebAPI.Models.Entities;
 using Xunit;
 
 namespace APITesting
@@ -6,9 +10,62 @@ namespace APITesting
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GetUserById()
         {
+            var options = new DbContextOptionsBuilder<tuliadbContext>()
+               .UseInMemoryDatabase(databaseName: "TuliaDatabase")
+               .Options;
 
+            using (var context = new tuliadbContext(options))
+            {
+                context.Users.Add(new WebAPI.Models.Entities.User {
+                    Id = 1,
+                    Username = "Liam",
+                    Password = "Password",
+                    FirstName = "Liam",
+                    LastName = "Sloan",
+                    NumberGroups = 0,
+                    Role = "user"
+                });
+                context.SaveChanges();
+
+                TuliaRepo repo = new TuliaRepo(context);
+
+                var user = new WebAPI.Models.DBModels.User(1, "Liam", "Sloan", "Liam");
+                var result = repo.GetUserById(1);
+
+                Assert.Equal(user.Username, result.Result.Username);
+            }
+        }
+
+        [Fact]
+        public void ReturnNullForInvalidUser()
+        {
+            var options = new DbContextOptionsBuilder<tuliadbContext>()
+               .UseInMemoryDatabase(databaseName: "TuliaDatabase")
+               .Options;
+
+            using (var context = new tuliadbContext(options))
+            {
+                context.Users.Add(new WebAPI.Models.Entities.User
+                {
+                    Id = 2,
+                    Username = "Liam",
+                    Password = "Password",
+                    FirstName = "Liam",
+                    LastName = "Sloan",
+                    NumberGroups = 0,
+                    Role = "user"
+                });
+                context.SaveChanges();
+
+                TuliaRepo repo = new TuliaRepo(context);
+
+                var user = new WebAPI.Models.DBModels.User(2, "greg", "Sloan", "Liam");
+                var result = repo.GetUserById(1);
+
+                Assert.NotEqual(user, result.Result);
+            }
         }
     }
 }
